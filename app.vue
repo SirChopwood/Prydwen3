6dda63012d9434ad69b6e9e2307025478170ed1b
Ramiris <sirchopwood@gmail.com>
2025-03-04 17:23:06 +0000

    Added Mobile layout 2

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@ useHead({
 
 <template>
   <div class="font-cabin h-full">
-    <div id="Background" class="fixed top-0 bg-gradient-to-b from-neutral-950 to-neutral-800 w-full h-full -z-50">
+    <div id="Background" class="fixed top-0 bg-gradient-to-b from-neutral-950 to-neutral-800 w-screen h-screen -z-50">
       <!-- Background stuff here -->
     </div>
       <NuxtLayout>
