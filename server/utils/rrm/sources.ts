b0744aa6b32abfaecbd82d285769f695f0bfeb4c
Ramiris <sirchopwood@gmail.com>
2025-09-05 03:10:14 +0100

    Revert "Hello YouTube, welcome to my lets play!"
    This reverts commit 9ca10158e9412ac28863202ee14eeda73c7593f0.

--- server/utils/rrm/sources.ts
+++ server/utils/rrm/sources.ts
@@ -1,5 +1,3 @@
-import {default as YouTubeTs} from "youtube.ts"
-
 export const Sources: Record<string, (request: string) => Promise<{
     text: string,
     code: string,
@@ -7,7 +5,6 @@ export const Sources: Record<string, (request: string) => Promise<{
 } | undefined>> = {
     "PyPy": PyPy,
     "PlainText": PlainText,
-    "YouTube": YouTube
 }
 
 export async function PyPy(request: string) {
@@ -41,16 +38,6 @@ export async function PyPy(request: string) {
                 requestData.metadata["Source"] = "PyPy"
                 requestData.metadata["Group"] = data.groups[song.group]
                 requestData.metadata["Duration"] = String(song.end - song.start)
-                try {
-                    let yt = new YouTubeTs(process.env.YT_TOKEN)
-                    let video = await yt.videos.get(song.originalUrl[0])
-                    if (video) {
-                        requestData.metadata["Thumbnail"] = video.snippet.thumbnails.default.url
-                    }
-                } catch (e) {
-                    console.log("Could not find thumbnail")
-                }
-
                 console.log(`Processed ${request} as PyPy.`)
                 return requestData
             }
@@ -66,28 +53,5 @@ export async function PlainText(request: string) {
     requestData.code = request
     requestData.metadata["Source"] = "PlainText"
     console.log(`Processed ${request} as Plain Text.`)
-    return requestData
-}
-
-export async function YouTube(request: string) {
-    const ytRegex = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/gi
-    let ytRequest = ytRegex.exec(request)
-    if (!ytRequest) {return undefined}
-    let yt = new YouTubeTs(process.env.YT_TOKEN)
-    let video = await yt.videos.get(ytRequest[0])
-    if (!video) {return undefined}
-
-
-    let requestData = {
-        text: video.snippet.title,
-        code: `https://www.youtube.com/watch?v=${video.id}`,
-        metadata: {
-            "Source": "YouTube",
-            "Duration": video.contentDetails.duration,
-            "Thumbnail": video.snippet.thumbnails.default.url,
-            "Channel": video.snippet.channelTitle
-        }
-    }
-    console.log(`Processed ${request} as YouTube.`)
     return requestData
 }
